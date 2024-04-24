ae67d74086cb8ccfe4cd1a0fd2057deabb0bda92
crusty0gphr <harutyun.mardirossian@armenotech.com>
2024-04-25 00:34:44 +0400

    lookup table + tests

--- src/lookup_table.rs
+++ src/lookup_table.rs
@@ -1,3 +1,11 @@
+// Basic implementation of the b64 lookup table with 64 chars
+// using basic addition and subtraction
+//
+// Example: ascii_c(A) -> b64_c(A) =>
+//          i := ascii_i(65) - b64_upper_i =>
+//          65 - 65 =>
+//          b65_i(0) == 'A'
+
 pub trait Lookup {
     // return the character for the given index - from base64 lookup table
     fn get_b64_char(&self, index: i8) -> Option<char>;
@@ -6,12 +14,14 @@ pub trait Lookup {
     fn get_padding(&self) -> char;
 }
 
+// shift to b65 indices
 const LOOKUP_OFFSET_UPPER: i8 = 65;
 const LOOKUP_OFFSET_LOWER: i8 = 71;
 const LOOKUP_OFFSET_NUM: i8 = 4;
 
 const PADDING_CHAR: char = '=';
 
+// zero value struct as a base type
 pub struct LookupTable;
 
 impl Lookup for LookupTable {
@@ -47,5 +57,68 @@ impl Lookup for LookupTable {
 
     fn get_padding(&self) -> char {
         PADDING_CHAR
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn lookup_get_b64_char() {
+        // upper case chars
+        {
+            let i: i8 = 12;
+            let exp: char = (i + LOOKUP_OFFSET_UPPER) as u8 as char;
+
+            let res = LookupTable.get_b64_char(i);
+            assert_eq!(res.unwrap(), exp);
+        }
+
+        // lower case chars
+        {
+            let i: i8 = 44;
+            let exp: char = (i + LOOKUP_OFFSET_LOWER) as u8 as char;
+
+            let res = LookupTable.get_b64_char(i);
+            assert_eq!(res.unwrap(), exp);
+        }
+
+        // None
+        {
+            let i: i8 = 127;
+
+            let res = LookupTable.get_b64_char(i);
+            assert_eq!(res, None);
+        }
+    }
+
+    #[test]
+    fn lookup_get_b64_index() {
+        // upper case chars
+        {
+            let ch: char = 'E';
+            let exp: i8 = ch as i8 - LOOKUP_OFFSET_UPPER;
+
+            let res = LookupTable.get_b64_index(ch);
+            assert_eq!(res.unwrap(), exp);
+        }
+
+        // lower case chars
+        {
+            let ch: char = 'o';
+            let exp: i8 = ch as i8 - LOOKUP_OFFSET_LOWER;
+
+            let res = LookupTable.get_b64_index(ch);
+            assert_eq!(res.unwrap(), exp);
+        }
+
+        // None
+        {
+            let ch: char = '~';
+
+            let res = LookupTable.get_b64_index(ch);
+            assert_eq!(res, None);
+        }
     }
 }
